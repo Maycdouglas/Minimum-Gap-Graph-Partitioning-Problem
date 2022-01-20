a3b5e729cc880db9c3b7c9db9316871bdf523a2e
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2022-01-20 15:34:57 -0300

    cria arquivos do clion

---
+++ cmake-build-debug/CMakeFiles/MGGPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MGGPP.dir/main.cpp.obj"
+  "MGGPP.exe"
+  "MGGPP.exe.manifest"
+  "MGGPP.pdb"
+  "libMGGPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MGGPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
